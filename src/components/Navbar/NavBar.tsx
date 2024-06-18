a966592bbad39968159e942f9c204973db7efd1d
shathukshan <shathukshanjeyakumar@gmail.com>
2024-06-18 19:57:59 +0530

    hot fix

--- src/components/Navbar/NavBar.tsx
+++ src/components/Navbar/NavBar.tsx
@@ -1,5 +1,33 @@
 import React from "react";
 
 export default function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <div>
+      {/* navbar */}
+
+      <nav className="navbar">
+        <h1>XorCodes</h1>
+
+        <div className="navlinks">
+          <li className="navlink">
+            <a href="#">Discover</a>
+          </li>
+          <li className="navlink">
+            <a href="#">Portfolio</a>
+          </li>
+          <li className="navlink">
+            <a href="#">Team</a>
+          </li>
+          <li className="navlink">
+            <a href="#">Careers</a>
+          </li>
+          <div id="contact">
+            <li className="navlink">
+              <a href="#">Contact us</a>
+            </li>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 }
